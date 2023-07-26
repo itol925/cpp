98501fb2f4939014707631381a2cb5ddfe64624c
panyinglong <panyinglong@meituan.com>
2023-07-26 20:42:37 +0800

    multi thread

--- src/continuable/continuable.hpp
+++ src/continuable/continuable.hpp
@@ -20,18 +20,73 @@ namespace continue_demo {
         //sudo make install
     }
 
-    using namespace boost;
+    namespace thread_demo {
+        using namespace boost;
+        void test() {
+            asio::io_context io_context;
+            asio::post(io_context, cti::use_continuable).then([] {
+                std::this_thread::sleep_for(std::chrono::seconds(5));
+            });
+            std::thread t([&io_context] {
+                std::cout << "context start.. " << std::endl;
+                io_context.run();
+                std::cout << "context start end" << std::endl;
+            });
+            t.join();
+        }
+
+        void test2() {
+            // 一个 context 接收任务派发给多个 thread 执行的情况。
+            // 一个 post 任务只会派给一个线程，不会重复执行。
+            // continuable 任务多个 then() 串联时，所有 then 回调由一个线程处理
+            asio::io_context io_context;
+            auto work = asio::make_work_guard(io_context);
+            for (int i = 0; i < 10; i++) { // 添加 10 串行个任务
+                asio::post(io_context, [i]{
+                    std::cout << std::this_thread::get_id() << ".task0." << i << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                });
+            }
+
+            asio::post(io_context, cti::use_continuable).then([] {
+                std::cout << std::this_thread::get_id() << ".task1" << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }).then([] {
+                std::cout << std::this_thread::get_id() << ".task2" << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }).then([] {
+                std::cout << std::this_thread::get_id() << ".task3" << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }).then([] {
+                std::cout << std::this_thread::get_id() << ".task4" << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }).then([] {
+                std::cout << std::this_thread::get_id() << ".task5" << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            });
+            asio::thread_pool thread_pool(5);
+            for (int i = 0; i < 3; ++i) {
+                asio::post(thread_pool, [&io_context, &thread_pool]() {
+                    try {
+                        io_context.run();
+                    } catch (const std::exception &e) {
+                        thread_pool.stop();
+                    }
+                });
+            }
+            for (int i = 0; i < 20; i++) { // 添加 10 串行个任务
+                asio::post(io_context, [i]{
+                    std::cout << std::this_thread::get_id() << ".task1." << i << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                });
+            }
+            work.reset();
+            thread_pool.join();
+        }
+    }
+
     void test() {
-        asio::io_context io_context;
-        asio::post(io_context, cti::use_continuable).then([] {
-            std::this_thread::sleep_for(std::chrono::seconds(5));
-        });
-        std::thread t([&io_context] {
-            std::cout << "context start.. " << std::endl;
-            io_context.run();
-            std::cout << "context start end" << std::endl;
-        });
-        t.join();
+        thread_demo::test2();
     }
 }
 
