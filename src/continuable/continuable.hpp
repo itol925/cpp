a99e43777c0f08e79418feb0e460e10b86f5926e
panyinglong <panyinglong@meituan.com>
2023-07-26 15:15:40 +0800

    continuable

---
+++ src/continuable/continuable.hpp
@@ -0,0 +1,21 @@
+#include <continuable/continuable.hpp>
+
+namespace continue_demo {
+    namespace install {
+        // git clone https://github.com/Naios/continuable.git
+        // git submodule init
+        // git submodule update
+
+        //cd continuable
+        //mkdir build
+        //cd build
+        //cmake ..
+
+        //make
+        //sudo make install
+    }
+
+//    cti::continuable<int>
+}
+
+
